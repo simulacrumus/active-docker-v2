8964a2e8713807d34b9faf4db06eab0d40e9897f
Emrah <emrahkinay@gmail.com>
2024-04-22 23:35:21 -0400

    db engine modified

--- scraper/db/engine.py
+++ scraper/db/engine.py
@@ -2,15 +2,13 @@ import sqlalchemy
 from os import environ as env
 
 def create_db_engine():
-    # connection_string = 'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'.format(
-    #     MYSQL_USER = env['MYSQL_USER'],
-    #     MYSQL_PASSWORD = env['MYSQL_PASSWORD'],
-    #     MYSQL_HOST = env['MYSQL_HOST'],
-    #     MYSQL_PORT = env['MYSQL_PORT'],
-    #     MYSQL_DATABASE = env['MYSQL_DATABASE']
-    # )
-
-    connection_string = 'mysql+mysqlconnector://admin:24DbDy97wRt7ztL4bUzrZZd7y@142.93.157.9:3306/active'
+    connection_string = 'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'.format(
+        MYSQL_USER = env['MYSQL_USER'],
+        MYSQL_PASSWORD = env['MYSQL_PASSWORD'],
+        MYSQL_HOST = env['MYSQL_HOST'],
+        MYSQL_PORT = env['MYSQL_PORT'],
+        MYSQL_DATABASE = env['MYSQL_DATABASE']
+    )
 
     engine = sqlalchemy.create_engine(connection_string,echo=False, future=True)
     return engine
